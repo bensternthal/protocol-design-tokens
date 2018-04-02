b3c9c3143445efb9cb732e121bc0887ae51220f8
Michael McCombie <michael@michaelm.cc>
2018-04-02 15:28:36 -0700

    Update Version Number

--- protocol-colors/protocol-colors.js
+++ protocol-colors/protocol-colors.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* Protocol Colors JS Variables v3.0.1 */
+/* Protocol Colors JS Variables v1.0.0 */
 
 exports.BLACK = '#000000';
 exports.PRIMARY = '#000000';
