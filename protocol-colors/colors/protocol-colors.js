92e57576bdbf4ce6e900a003a930c993ede3fb15
Michael McCombie <michael@michaelm.cc>
2018-04-03 16:50:19 -0700

    Update colors (#7)
    * Update Colors
    * Update version number

--- protocol-colors/colors/protocol-colors.js
+++ protocol-colors/colors/protocol-colors.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* Protocol Colors JS Variables v1.0.0 */
+/* Protocol Colors JS Variables v1.1.0 */
 
 exports.BLACK = '#000000';
 exports.PRIMARY = '#000000';
@@ -10,31 +10,35 @@ exports.PRIMARY = '#000000';
 exports.WHITE = '#ffffff';
 exports.SECONDARY = '#ffffff';
 
-exports.CYAN_LIGHT = '#00ffff';
-exports.CYAN = '#00e5e5';
-exports.CYAN_DARK = '#00cccc';
+exports.CYAN_LIGHT = '#a2ffff';
+exports.CYAN = '#00ffff';
+exports.CYAN_DARK = '#00e5e5';
 
-exports.YELLOW_LIGHT = '#fff63a';
-exports.YELLOW = '#e5dc34';
-exports.YELLOW_DARK = '#ccc42e';
+exports.YELLOW_LIGHT = '#fff9a3';
+exports.YELLOW = '#fff44f';
+exports.YELLOW_DARK = '#f7e800';
 
-exports.RED_LIGHT = '#ff4d4e';
-exports.RED = '#e54546';
-exports.RED_DARK = '#cc3d3e';
+exports.RED_LIGHT = '#fe7c87';
+exports.RED = '#ff4f5e';
+exports.RED_DARK = '#e53c4a';
 
-exports.GREEN_LIGHT = '#49ffbb';
-exports.GREEN = '#41e5a7';
-exports.GREEN_DARK = '#3acc95';
+exports.GREEN_LIGHT = '#adffdf';
+exports.GREEN = '#54ffbd';
+exports.GREEN_DARK = '#2ce69e';
 
-exports.TEAL_LIGHT = '#00a7a7';
-exports.TEAL = '#009696';
-exports.TEAL_DARK = '#008484';
+exports.TEAL_LIGHT = '#25bcbc';
+exports.TEAL = '#0f8f8f';
+exports.TEAL_DARK = '#005e5e';
 
-exports.PURPLE_LIGHT = '#cf2cfa';
-exports.PURPLE = '#ba27e1';
-exports.PURPLE_DARK = '#a423c7';
+exports.PURPLE_LIGHT = '#9f54b2';
+exports.PURPLE = '#6e008b';
+exports.PURPLE_DARK = '#4f0164';
 
-exports.BLUE_LIGHT = '#00a7e0';
-exports.BLUE = '#0096ca';
-exports.BLUE_DARK = '#0084b2';
+exports.BLUE_LIGHT = '#539ee9';
+exports.BLUE = '#2374c6';
+exports.BLUE_DARK = '#00458b';
+
+exports.GRAY_LIGHT = '#e7e7e7';
+exports.GRAY = '#c8c8c8';
+exports.GRAY_DARK = '#959595';
 
--- protocol-colors/colors/protocol-colors.swift
+++ protocol-colors/colors/protocol-colors.swift
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* Protocol Colors iOS Variables v1.0.0
+/* Protocol Colors iOS Variables v1.1.0
    From https://github.com/mozilla/protocol-design-tokens/tree/master/protocol-colors#readme */
 
 extension UIColor {
@@ -13,33 +13,37 @@ extension UIColor {
         static let White = UIColor(rgb: 0xffffff)
         static let Secondary = UIColor(rgb: 0xffffff)
 
-        static let Cyanlight = UIColor(rgb: 0x00ffff)
-        static let Cyan = UIColor(rgb: 0x00e5e5)
-        static let Cyandark = UIColor(rgb: 0x00cccc)
+        static let Cyanlight = UIColor(rgb: 0xa2ffff)
+        static let Cyan = UIColor(rgb: 0x00ffff)
+        static let Cyandark = UIColor(rgb: 0x00e5e5)
 
-        static let Yellowlight = UIColor(rgb: 0xfff63a)
-        static let Yellow = UIColor(rgb: 0xe5dc34)
-        static let Yellowdark = UIColor(rgb: 0xccc42e)
+        static let Yellowlight = UIColor(rgb: 0xfff9a3)
+        static let Yellow = UIColor(rgb: 0xfff44f)
+        static let Yellowdark = UIColor(rgb: 0xf7e800)
 
-        static let Redlight = UIColor(rgb: 0xff4d4e)
-        static let Red = UIColor(rgb: 0xe54546)
-        static let Reddark = UIColor(rgb: 0xcc3d3e)
+        static let Redlight = UIColor(rgb: 0xfe7c87)
+        static let Red = UIColor(rgb: 0xff4f5e)
+        static let Reddark = UIColor(rgb: 0xe53c4a)
 
-        static let Greenlight = UIColor(rgb: 0x49ffbb)
-        static let Green = UIColor(rgb: 0x41e5a7)
-        static let Greendark = UIColor(rgb: 0x3acc95)
+        static let Greenlight = UIColor(rgb: 0xadffdf)
+        static let Green = UIColor(rgb: 0x54ffbd)
+        static let Greendark = UIColor(rgb: 0x2ce69e)
 
-        static let Teallight = UIColor(rgb: 0x00a7a7)
-        static let Teal = UIColor(rgb: 0x009696)
-        static let Tealdark = UIColor(rgb: 0x008484)
+        static let Teallight = UIColor(rgb: 0x25bcbc)
+        static let Teal = UIColor(rgb: 0x0f8f8f)
+        static let Tealdark = UIColor(rgb: 0x005e5e)
 
-        static let Purplelight = UIColor(rgb: 0xcf2cfa)
-        static let Purple = UIColor(rgb: 0xba27e1)
-        static let Purpledark = UIColor(rgb: 0xa423c7)
+        static let Purplelight = UIColor(rgb: 0x9f54b2)
+        static let Purple = UIColor(rgb: 0x6e008b)
+        static let Purpledark = UIColor(rgb: 0x4f0164)
 
-        static let Bluelight = UIColor(rgb: 0x00a7e0)
-        static let Blue = UIColor(rgb: 0x0096ca)
-        static let Bluedark = UIColor(rgb: 0x0084b2)
+        static let Bluelight = UIColor(rgb: 0x539ee9)
+        static let Blue = UIColor(rgb: 0x2374c6)
+        static let Bluedark = UIColor(rgb: 0x00458b)
+
+        static let Graylight = UIColor(rgb: 0xe7e7e7)
+        static let Gray = UIColor(rgb: 0xc8c8c8)
+        static let Graydark = UIColor(rgb: 0x959595)
 
   }
 }
