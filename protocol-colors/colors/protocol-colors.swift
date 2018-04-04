605060bb2743afaa72eb0579e7e90b0550f3476a
Michael McCombie <michael@michaelm.cc>
2018-04-04 16:33:43 -0700

    Update Version Number

--- protocol-colors/colors/protocol-colors.swift
+++ protocol-colors/colors/protocol-colors.swift
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* Protocol Colors iOS Variables v1.1.1
+/* Protocol Colors iOS Variables v1.1.2
    From https://github.com/mozilla/protocol-design-tokens/tree/master/protocol-colors#readme */
 
 extension UIColor {
