b4a4e0af2d53baf528868bdbc5c78964f7251a54
Alex Gibson <alexgibson@users.noreply.github.com>
2018-04-30 14:54:31 +0100

    Lint JS, CSS and add EditorConfig

--- protocol-layout/protocol-layout.js
+++ protocol-layout/protocol-layout.js
@@ -4,20 +4,20 @@
 
 /* Protocol Colors JS Variables v2.0.0 */
 
-exports.SPACING_XS: "4px"
-exports.SPACING_SM: "8px"
-exports.SPACING_MD: "16px"
-exports.SPACING_LG: "24px"
-exports.SPACING_XL: "32px"
-exports.SPACING_XXL: "48px"
+exports.SPACING_XS = '4px';
+exports.SPACING_SM = '8px';
+exports.SPACING_MD = '16px';
+exports.SPACING_LG = '24px';
+exports.SPACING_XL = '32px';
+exports.SPACING_XXL = '48px';
 
 
 
-exports.LAYOUT_XXS: "16px"
-exports.LAYOUT_XS: "24px"
-exports.LAYOUT_SM: "32px"
-exports.LAYOUT_MD: "48px"
-exports.LAYOUT_LG: "64px"
-exports.LAYOUT_XL: "96px"
-exports.LAYOUT_XXL: "160px"
+exports.LAYOUT_XXS = '16px';
+exports.LAYOUT_XS = '24px';
+exports.LAYOUT_SM = '32px';
+exports.LAYOUT_MD = '48px';
+exports.LAYOUT_LG = '64px';
+exports.LAYOUT_XL = '96px';
+exports.LAYOUT_XXL = '160px';
 
